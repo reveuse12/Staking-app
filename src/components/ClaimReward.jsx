040aa030faef420e0c52bbe17e9636c8ab17727e
prayag <prayag129787@gmail.com>
2023-12-18 22:03:22 +0530

    improve error handling

--- src/components/ClaimReward.jsx
+++ src/components/ClaimReward.jsx
@@ -1,28 +1,57 @@
+import { toast } from "react-hot-toast";
 import { useState } from "react";
 import { Web3Button } from "@thirdweb-dev/react";
 
 const ClaimReward = () => {
   const [claimRewards, setClaimRewards] = useState(0);
 
+  const handleClaim = async (contract) => {
+    try {
+      validateClaimAmount(claimRewards);
+
+      await toast.promise(contract.call("claimRewards"));
+      toast.success("Claim successful");
+      setClaimRewards(0);
+    } catch (error) {
+      handleClaimError(error);
+    }
+  };
+
+  const validateClaimAmount = (amount) => {
+    if (!amount || isNaN(amount) || amount <= 0) {
+      throw new Error("Please enter a valid amount to claim.");
+    }
+  };
+
+  const handleClaimError = (error) => {
+    console.error("Error claiming rewards:", error);
+
+    let errorMessage =
+      "An error occurred while claiming rewards. Please try again.";
+
+    if (error.message) {
+      errorMessage = error.message;
+    }
+
+    toast.error(errorMessage);
+  };
+
   return (
     <div className="mx-auto w-full text-center bg-gradient-to-br from-[#262a33] to-[#19202a] mt-8 rounded-lg p-6">
       <h2 className="text-3xl font-bold mb-4 text-[#f43550] ">
         Claim Your Web3 Rewards Tokens
       </h2>
-      <div className="flex bg-[#5d97a8] p-4  rounded-lg shadow-md mb-4 items-center justify-center">
+      <div className="flex bg-[#5d97a8] p-4 rounded-lg shadow-md mb-4 items-center justify-center">
         <input
-          type="Number"
+          type="number"
           placeholder="Enter Amount"
           value={claimRewards}
           onChange={(e) => setClaimRewards(e.target.value)}
-          className="p-3 mr-2 border text-white border-gray-300 rounded focus:outline-none focus:border-white bg-gray-700 "
+          className="p-3 mr-2 border text-white border-gray-300 rounded focus:outline-none focus:border-white bg-gray-700"
         />
         <Web3Button
           contractAddress={import.meta.env.VITE_STAKING_CONTRACT_ADDRESS}
-          action={(contract) => {
-            contract.call("claimRewards");
-            setClaimRewards(0);
-          }}
+          action={handleClaim}
           className="bg-pink-700 text-white p-2 rounded cursor-pointer hover:bg-pink-700 hover:text-white"
         >
           Claim
--- src/components/stake.jsx
+++ src/components/stake.jsx
@@ -7,56 +7,69 @@ const Stake = () => {
   const [stakeAmount, setStakeAmount] = useState(0);
   const [withdrawAmount, setWithdrawAmount] = useState(0);
 
+  const handleStake = async (contract, amount, actionType) => {
+    try {
+      if (!amount || isNaN(amount) || amount <= 0) {
+        throw new Error(`Please enter a valid amount to ${actionType}.`);
+      }
+
+      await contract
+        .call(actionType, [ethers.utils.parseEther(amount)])
+        .then(() => toast.success(`${actionType} successfully`));
+
+      // Reset the input field
+      actionType === "stake" ? setStakeAmount(0) : setWithdrawAmount(0);
+    } catch (error) {
+      console.error(`Error ${actionType}ing:`, error);
+
+      let errorMessage = `An error occurred while ${actionType}ing. Please try again.`;
+
+      if (error.message) {
+        errorMessage = error.message;
+      }
+
+      toast.error(errorMessage);
+    }
+  };
+
   return (
     <div className="text-center w-full mt-8 p-6 bg-gradient-to-br from-[#262a33] to-[#19202a] text-white rounded-lg shadow-md">
       <h2 className="text-3xl font-bold text-[#f43550] mb-4">
         Stake your Web3 Token
       </h2>
 
-      <div className="bg-[#5d97a8] p-4 rounded-md mb-4">
-        <input
-          type="Number"
-          placeholder="Enter Amount"
-          value={stakeAmount}
-          onChange={(e) => setStakeAmount(e.target.value)}
-          className="p-3 mr-2 bg-gray-700 text-white rounded-md"
-        />
-        <Web3Button
-          contractAddress={import.meta.env.VITE_STAKING_CONTRACT_ADDRESS}
-          action={(contract) => {
-            contract.call("stake", [ethers.utils.parseEther(stakeAmount)]);
-            setStakeAmount(0);
-          }}
-          onSuccess={() => {
-            toast("Staked Successfully");
-          }}
-          className="mt-2 bg-green-500 hover:text-white hover:bg-green-600 text-white py-2 px-4 rounded-md cursor-pointer"
-        >
-          Stake
-        </Web3Button>
-      </div>
-
-      <div className="bg-[#5d97a8] p-4 rounded-md">
-        <input
-          type="Number"
-          placeholder="Enter Amount"
-          value={withdrawAmount}
-          onChange={(e) => setWithdrawAmount(e.target.value)}
-          className="p-3 mr-2 bg-gray-700 text-white rounded-md"
-        />
-        <Web3Button
-          contractAddress={import.meta.env.VITE_STAKING_CONTRACT_ADDRESS}
-          action={(contract) => {
-            contract.call("withdraw", [
-              ethers.utils.parseEther(withdrawAmount),
-            ]);
-            setWithdrawAmount(0);
-          }}
-          className="mt-2 bg-red-500 hover:text-white hover:bg-red-600 text-white py-2 px-4 rounded-md cursor-pointer"
-        >
-          Withdraw
-        </Web3Button>
-      </div>
+      {["Stake", "Withdraw"].map((actionType, index) => (
+        <div key={index} className="bg-[#5d97a8] p-4 rounded-md mb-4">
+          <input
+            type="Number"
+            placeholder={`Enter ${actionType} Amount`}
+            value={actionType === "Stake" ? stakeAmount : withdrawAmount}
+            onChange={(e) =>
+              actionType === "Stake"
+                ? setStakeAmount(e.target.value)
+                : setWithdrawAmount(e.target.value)
+            }
+            className="p-3 mr-2 bg-gray-700 text-white rounded-md"
+          />
+          <Web3Button
+            contractAddress={import.meta.env.VITE_STAKING_CONTRACT_ADDRESS}
+            action={(contract) =>
+              handleStake(
+                contract,
+                actionType === "Stake" ? stakeAmount : withdrawAmount,
+                actionType.toLowerCase()
+              )
+            }
+            className={`mt-2 ${
+              actionType === "Stake"
+                ? "bg-green-500 hover:bg-green-600"
+                : "bg-red-500 hover:bg-red-600"
+            } text-white py-2 px-4 rounded-md cursor-pointer`}
+          >
+            {actionType}
+          </Web3Button>
+        </div>
+      ))}
     </div>
   );
 };
